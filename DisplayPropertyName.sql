c8d214e7b99158a59795451cf501e3da038478ef
Obinna Nweke <nweob294@student.otago.ac.nz>
2020-12-07 14:47:08 +1300

    Updated query to show all properties for owner 1426

--- DisplayPropertyName.sql
+++ DisplayPropertyName.sql
@@ -1,22 +1,14 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[PropertyTypeId]
-      ,[AddressId]
-      ,[Name]
-      ,[Description]
-      ,[Bedroom]
-      ,[Bathroom]
-      ,[LandSqm]
-      ,[ParkingSpace]
-      ,[FloorArea]
-      ,[TargetRent]
-      ,[TargetRentTypeId]
-      ,[YearBuilt]
-      ,[IsActive]
-      ,[CreatedOn]
-      ,[CreatedBy]
-      ,[UpdatedOn]
-      ,[UpdatedBy]
-      ,[HasCompany]
-      ,[IsOwnerOccupied]
-  FROM [Keys].[dbo].[Property]
+/*
+
+This query displays a list of all property names and their property id’s for Owner Id: 1426. 
+
+*/
+
+SELECT Property.Name AS PropertyName, Property.Id AS PropertyId
+FROM Property
+INNER JOIN OwnerProperty
+ON Property.Id = OwnerProperty.PropertyId
+WHERE OwnerProperty.OwnerId = 1426;
+
+
+
